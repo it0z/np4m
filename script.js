2cf57d0c7dbad3e653d73d900ad345165cfd281d
it0z <ionic-tier0z@icloud.com>
2026-05-04 17:09:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function testExtF(){
   document.title = "ext js test.";
 }
+
+function clock1(){
+  
+  const now = new Date();
+  
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const strHours = hours < 10 ? '0' + hours : hours;
+  const strMinutes = minutes < 10 ? '0' + minutes : minutes;
+  const strSeconds = seconds < 10 ? '0' + seconds : seconds;
+  
+  document.getElementById('clock1').innerText = '${strHours}:${strMinutes}:${strSeconds}';
+  
+}
