c1286c403fd40e7dc961a1df903e22e32a656c08
it0z <ionic-tier0z@icloud.com>
2026-05-17 17:45:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,75 @@
 function testExtF(){
-  document.title = "Our Pets 🐦";
+  document.title = "Our Pets";
 }
+
+
+const favLink = document.createElement('link');
+favLink.rel = 'icon';
+favLink.href = 'redBird.png';
+document.head.appendChild(favLink);
+
+
+const custCursor = document.createElement('link');
+custCursor.rel = 'stylesheet';
+custCursor.href = 'https://cdn.cursors-4u.net/cursors/animated/cool-pink-pointer-glitter-3488f1ab-32.css';
+document.head.appendChild(custCursor);
+
+
+const topBannerHTML = `
+	<div class="dropdown" onclick="dropdownF('dd1')">
+		JellyNeo Links
+		<div id="dd1" class="dropdown-content">
+
+			<a href="https://www.jellyneo.net/?go=the_void_within&id=essence_collection" target="_blank">Void Essence</a>
+			<a href="https://items.jellyneo.net/mywishes/modojn/402315/" target="_blank">Codestones</a>
+			<a href="https://items.jellyneo.net/mywishes/modojn/404181/" target="_blank">Nerkmids</a>
+			<a href="https://items.jellyneo.net/mywishes/modojn/402320/" target="_blank">Neopet PBs</a>
+			<a href="https://items.jellyneo.net/mywishes/modojn/498429/" target="_blank">Petpet PBs</a>
+
+  		</div>
+	</div>
+	<div class="dropdown" onclick="dropdownF('dd2')">
+		Other Links
+		<div id="dd2" class="dropdown-content">
+
+			<a href="https://v.angelcam.com/iframe?v=16lbkvzpl4&autoplay=1" target="_blank">Mama Angel</a>
+			<a href="https://impress-2020.openneo.net/" target="_blank" title="Opens Dress to Impress in a new tab ⤴">Dress to Impress</a>
+			<a href="testpage.html" title="#">Test Page</a>
+			<a href="resources.html" title="#">Resources</a>
+			<a href="bd.html" title="#">BD Corner</a>
+			<a href="stones.html" title="#">Codestone Tool</a>
+
+  		</div>
+	</div>
+	<div class="dropdown" onclick="dropdownF('dd3')">
+		Neopets Dailies
+		<div id="dd3" class="dropdown-content">
+
+			<a href="https://www.neopets.com/halloween/haunted_woods_hunt.phtml" target="_blank">Haunted Woods Hunt</a>
+			<a href="https://www.neopets.com/trudys_surprise.phtml" target="_blank">Trudy's Surprise</a>
+			<a href="https://www.neopets.com/premium/wheel.phtml" target="_blank">The Wheel of Starlight!</a>
+			<a href="https://www.neopets.com/medieval/earthfaerie.phtml" target="_blank">Illusen's Glade</a>		
+			<a href="https://www.neopets.com/faerieland/employ/employment.phtml" target="_blank">Faerieland Employment Agency</a>		
+			<a href="https://www.neopets.com/games/lottery.phtml" target="_blank">Neopian Lottery</a>    
+
+  		</div>
+	</div>
+	<div class="dropdown" onclick="dropdownF('dd4')">
+		Testing Area
+		<div id="dd4" class="dropdown-content">
+
+			<a href="db.html">Leave a Comment</a> 
+			<a href="food.html">Food Archive</a>      
+
+  		</div>
+	</div>
+`;
+
+document.getElementById('topBanner').insertAdjacentHTML('beforeend', topBannerHTML);
+
+const siteLogoJS = '<a href="index.html">Our Pets </a><img src="icons/redBird.png">';
+document.getElementById('siteLogo').insertAdjacentHTML('beforeend', siteLogoJS);
+
 
 function clock1(){
   
